1d3b87c97b87d3154884a808c48502a32aab0c22
Mikkel Helding Vembye <mikkel.vembye@gmail.com>
2022-02-24 22:13:57 +0100

    Adding Vembye et al. data

---
+++ R/data-documentation.R
@@ -0,0 +1,28 @@
+#' Co-teaching dataset
+#'
+#' Meta-analytical dataset regarding the effects of collaborative models
+#' of instruction of student achievement from Vembye, Weiss, and
+#' Bhat (forthcoming)
+#'
+#' @format A tibble with 76 rows/studies and 9 variables
+#'\describe{
+#' \item{study_year}{Study author and year of publication}
+#' \item{studyid}{Unique study ID}
+#' \item{esid}{Unique effect size ID}
+#' \item{kj}{Number of effect sizes per study}
+#' \item{N_meanj}{Average sample size of studies}
+#' \item{Nt_meanj}{Average size of treatment group}
+#' \item{Nc_meanj}{Average size of control group}
+#' \item{ESS_meanj}{Roughly approximated effective sample sizes}
+#' \item{vg_ms_mean}{Cluster bias corrected sampling variances}
+#'}
+#'
+#'@source \href{https://bit.ly/3nhVX3H}{Vembye's OSF page}
+#'
+#' @references Vembye, Weiss, & Bhat (forthcoming). The Effects
+#' Co-Teaching and Related Collaborative Models of Instruction on
+#' Student Achievement: A Systematic Review and Meta-Analysis. Access
+#' to background material at <https://bit.ly/3nhVX3H>
+#'
+
+"VWB22_pilot"

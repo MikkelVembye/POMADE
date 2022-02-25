c172dea120ab9d167efc0e0513d557049ed83f4a
Mikkel Helding Vembye <mikkel.vembye@gmail.com>
2022-02-25 10:52:51 +0100

    Added traffic light power plot function

---
+++ R/traffic_light_power_plot.R
@@ -0,0 +1,48 @@
+#' @title Make traffic light power plot
+#'
+#' @param power_plot Facet grid \code{power_plot} object
+#' @param assumptions Assumption regarding the likelihood of the given scenario
+#' in the \code{power_plot}. Make assumptions from upper-left strip to lower-right strip
+#'
+#' @return Return a traffic light power plot
+#' @export
+#'
+#'
+
+
+
+traffic_light_power_plot <-
+  function(
+    power_plot, assumptions
+
+  ){
+
+
+
+    assump <-
+      dplyr::tibble(assumptions) %>%
+      dplyr::mutate(
+        color = dplyr::recode(
+          assumptions,
+          "unlikely" = "lightcoral",
+          "likely" = "lightgoldenrodyellow",
+          "expected" = "mediumaquamarine"
+        )
+      )
+
+    g <- ggplot2::ggplot_gtable(ggplot2::ggplot_build(power_plot))
+    strip_both <- which(grepl('strip-', g$layout$name))
+    fills <- assump$color
+
+    k <- 1
+    for (i in strip_both) {
+      j <- which(grepl('rect', g$grobs[[i]]$grobs[[1]]$childrenOrder))
+      g$grobs[[i]]$grobs[[1]]$children[[j]]$gp$fill <- fills[k]
+      k <- k+1
+    }
+
+    traffic_plot <- g
+    grid::grid.draw(traffic_plot)
+
+
+  }

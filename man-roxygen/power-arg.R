f5db85b90ed37e31e7d15429dd354e17e9f21695
Mikkel Helding Vembye <mikkel.vembye@gmail.com>
2022-04-13 10:40:46 +0200

    Minor changes in the documentation

--- man-roxygen/power-arg.R
+++ man-roxygen/power-arg.R
@@ -12,8 +12,8 @@
 #' @param N_dist Simulated distribution o
 #' @param pilot_data_kjN Pilot data containing kjs and effective sample sizes
 #' @param sigma2_mean Average sampling variance
-#' @param pilot_data_kjsigma2 Pilot data containing kjs and sigma2js
+#' @param pilot_data_kjsigma2 Pilot data containing vectors of kjs and sigma2js
 #' @param alpha statistical significance level
-#' @param iterations Number of iterations
+#' @param iterations Number of iterations (default = 100)
 #' @param average_power Average power across the x iterated power estimations
 #' @param seed Set seed to ensure reproducibility of the iterated power approximations

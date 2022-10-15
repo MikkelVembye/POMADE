2e5d9d8967c9cae403e90f42ddae7b02e78fccd5
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2022-10-15 22:10:58 +0200

    Smaller chagnes to power and adding plot function for MDES data

--- R/plot_MADE.R
+++ R/plot_MADE.R
@@ -47,7 +47,7 @@ plot_MADE <-
     limits <- limits
   }
 
-
+  xlab <- "Number of Studies (J)"
 
   if (any(colnames(data) == "power")){
 
@@ -56,6 +56,7 @@ plot_MADE <-
     } else {
       color_plot <- ggplot2::aes(x = J, y = power, shape = cor, linetype = cor)
     }
+
 
     if (is.null(expected_studies)){
 
@@ -92,11 +93,12 @@ plot_MADE <-
 
     }
 
+
     if (caption){
 
     plot <-
       plot_dat |>
-      group_by(model, mu, d, alpha) |>
+      group_by(mu, d, alpha, model) |>
       group_map(
         .f = ~ ggplot2::ggplot(data = .x) +
           color_plot +
@@ -109,14 +111,15 @@ plot_MADE <-
           ggplot2::theme_bw() +
           ggplot2::scale_x_continuous(breaks = breaks, limits = limits) +
           ggplot2::scale_y_continuous(breaks = seq(0, 1, .2), limits = c(0,1)) +
-          ggplot2::theme(legend.position = legend_position) +
           ggplot2::labs(
-            x = "Number of studies (J)",
+            x = xlab,
             y = paste0("Power", " (", unique(.y$model), ")"),
-            caption = paste0("Note: mu = ", unique(.y$mu), ", ", "d = ", unique(.y$d),
-                             ", and ", "alpha = ", unique(.y$alpha))
+            caption = paste0("Note: Effect size of practical concern = ", unique(.y$mu), ", ", "contrast values = ", unique(.y$d),
+                             ", and ", "alpha = ", unique(.y$alpha), ".")
           ) +
-          ggplot2::theme(plot.caption.position = "plot", plot.caption = ggplot2::element_text(hjust = 0)) +
+          ggplot2::theme(legend.position = legend_position,
+                         plot.caption.position = "plot",
+                         plot.caption = ggplot2::element_text(hjust = 0)) +
           ggplot2::expand_limits(y = 0)
       )
 
@@ -124,7 +127,7 @@ plot_MADE <-
 
       plot <-
         plot_dat |>
-        group_by(model, mu, d, alpha) |>
+        group_by(mu, d, alpha, model) |>
         group_map(
           .f = ~ ggplot2::ggplot(data = .x) +
             color_plot +
@@ -137,8 +140,8 @@ plot_MADE <-
             ggplot2::theme_bw() +
             ggplot2::scale_x_continuous(breaks = breaks, limits = limits) +
             ggplot2::scale_y_continuous(breaks = seq(0, 1, .2), limits = c(0,1)) +
+            ggplot2::labs(x = xlab, y = paste0("Power", " (", unique(.y$model), ")")) +
             ggplot2::theme(legend.position = legend_position) +
-            ggplot2::labs(x = "Number of studies (J)", y = paste0("Power", " (", unique(.y$model), ")")) +
             ggplot2::expand_limits(y = 0)
         )
 
@@ -148,31 +151,155 @@ plot_MADE <-
 
       label_length <- n_distinct(plot_dat$tau2)*n_distinct(plot_dat$omega2)
 
-      df_text <- tibble(
-        tau_name = factor(rep(unique(plot_dat$tau_name), n_distinct(plot_dat$omega_name))),
+      df_text <-
+        tibble(
+          tau_name = factor(rep(unique(plot_dat$tau_name), n_distinct(plot_dat$omega_name))),
 
-        omega_name = factor(rep(unique(plot_dat$omega_name), each = n_distinct(plot_dat$tau_name))),
+          omega_name = factor(rep(unique(plot_dat$omega_name), each = n_distinct(plot_dat$tau_name))),
 
-        label = paste0("(", 1:label_length, ")"),
+          label = paste0("(", 1:label_length, ")"),
 
-        cor = factor(unique(plot_dat$rho[1]))
-      )
+          cor = factor(unique(plot_dat$rho[1]))
+        )
 
       plot <- purrr::map(plot, ~ .x + ggplot2::geom_text(data = df_text, ggplot2::aes(x = max(plot_dat$J), y = .01, label = label),
                                              size = number_size, color = "black"))
 
     }
 
-
-    if (n_distinct(data$model) == 1) plot <- plot[[1]]
-
   }
 
   ####################################
-  # MDES plots
+  # MDES plot(s)
   ####################################
 
-  # Insert MDES plot function
+  if (any(colnames(data) == "MDES")){
+
+    if (color){
+      color_plot <- ggplot2::aes(x = J, y = MDES, colour = cor, shape = cor, linetype = cor)
+    } else {
+      color_plot <- ggplot2::aes(x = J, y = MDES, shape = cor, linetype = cor)
+    }
+
+
+    if (is.null(expected_studies)){
+
+      gray_shade <- NULL
+      vline <- NULL
+
+
+    } else if (length(expected_studies) == 1){
+
+      gray_shade <- NULL
+      vline <- ggplot2::geom_vline(xintercept = expected_studies, linetype = "dashed")
+
+
+    } else if (length(expected_studies) == 2){
+
+      gray_shade <- ggplot2::geom_rect(
+        ggplot2::aes(xmin = expected_studies[1], xmax = expected_studies[2],
+                     ymin = -Inf, ymax = Inf), fill = "gray90", color = "gray90", alpha = 0.1)
+
+      vline <- NULL
+
+
+    } else if (length(expected_studies) == 3){
+
+      gray_shade <- ggplot2::geom_rect(
+        ggplot2::aes(xmin = expected_studies[1], xmax = expected_studies[3],
+                     ymin = -Inf, ymax = Inf), fill = "gray90", color = "gray90", alpha = 0.1)
+
+      vline <- ggplot2::geom_vline(xintercept = expected_studies[2], linetype = "dashed")
+
+    } else if (length(expected_studies) > 3) {
+
+      stop("expected_studies must not exceed 3 values")
+
+    }
+
+
+    if (caption){
+
+      plot <-
+        plot_dat |>
+        group_by(d, alpha, target_power, model) |>
+        group_map(
+          .f = ~ ggplot2::ggplot(data = .x) +
+            color_plot +
+            gray_shade +
+            ggplot2::geom_point() +
+            ggplot2::geom_line() +
+            ggplot2::geom_hline(yintercept = MDES_min, linetype = "dashed") +
+            vline +
+            ggplot2::facet_grid(omega_name ~ tau_name) +
+            ggplot2::theme_bw() +
+            ggplot2::scale_x_continuous(breaks = breaks, limits = limits) +
+            ggplot2::scale_y_continuous(
+              breaks = seq(round(min(plot_dat$MDES) - 0.015, 2) , round(max(plot_dat$MDES) + 0.015, 2), .02),
+              limits = c(round(min(plot_dat$MDES) - 0.015, 2) , round(max(plot_dat$MDES) + 0.015, 2) )
+            ) +
+            ggplot2::labs(
+              x = xlab,
+              y = paste0("Minimum Detectable ES", " (", unique(.y$model), ")"),
+              caption = paste0("Note:", " Contrast value = ", unique(.y$d), ", ",
+                               "alpha = ", unique(.y$alpha), ", ", "target power = ", unique(.y$target_power), ".")
+            ) +
+            ggplot2::theme(legend.position = legend_position,
+                           plot.caption.position = "plot",
+                           plot.caption = ggplot2::element_text(hjust = 0))
+        )
+
+    } else {
+
+      plot <-
+        plot_dat |>
+        group_by(d, alpha, target_power, model) |>
+        group_map(
+          .f = ~ ggplot2::ggplot(data = .x) +
+            color_plot +
+            gray_shade +
+            ggplot2::geom_point() +
+            ggplot2::geom_line() +
+            ggplot2::geom_hline(yintercept = MDES_min, linetype = "dashed") +
+            vline +
+            ggplot2::facet_grid(omega_name ~ tau_name) +
+            ggplot2::theme_bw() +
+            ggplot2::scale_x_continuous(breaks = breaks, limits = limits) +
+            ggplot2::scale_y_continuous(
+              breaks = seq(round(min(plot_dat$MDES) - 0.015, 2) , round(max(plot_dat$MDES) + 0.015, 2), .02),
+              limits = c(round(min(plot_dat$MDES) - 0.015, 2) , round(max(plot_dat$MDES) + 0.015, 2) )
+            ) +
+            ggplot2::labs(x = xlab, y = paste0("Minimum Detectable ES", " (", unique(.y$model), ")")) +
+            ggplot2::theme(legend.position = legend_position)
+        )
+
+    }
+
+    if (numbers){
+
+      label_length <- n_distinct(plot_dat$tau2)*n_distinct(plot_dat$omega2)
+
+      df_text <-
+        tibble(
+          tau_name = factor(rep(unique(plot_dat$tau_name), n_distinct(plot_dat$omega_name))),
+
+          omega_name = factor(rep(unique(plot_dat$omega_name), each = n_distinct(plot_dat$tau_name))),
+
+          label = paste0("(", 1:label_length, ")"),
+
+          cor = factor(unique(plot_dat$rho[1]))
+        )
+
+      plot <- purrr::map(plot, ~ .x + ggplot2::geom_text(data = df_text,
+                                                         ggplot2::aes(x = max(plot_dat$J),
+                                                         y = round(max(plot_dat$MDES) + 0.015, 2), label = label),
+                                                         size = number_size, color = "black"))
+
+    }
+
+  }
+
+  if (length(plot) == 1) plot <- plot[[1]]
 
   plot
 

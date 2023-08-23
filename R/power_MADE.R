fc86c2a7b5243bc9eabe8c7781cd910f0061dd2b
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-08-23 11:43:41 +0200

    corrigendum text in code

--- R/power_MADE.R
+++ R/power_MADE.R
@@ -468,7 +468,7 @@ power_MADE_single <-
       # Estimating a presented in corrigendum
       a <- sum(1/sigma2j)
 
-      # Equation 17 in Vembye, Pustejovsky, & Pigott (2022)
+      # Equation 17 in Vembye, Pustejovsky, & Pigott (2023, corrigendum)
       tau2_e <- tau^2 + omega^2 * (1 - 1/a^2 * sum(1 / (kj * sigma2j^2))) / (1 - 1/a^2 * sum(1 / sigma2j^2))
 
       # _dd = dotdot
